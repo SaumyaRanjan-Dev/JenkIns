03f8071fa213c0d68ceedae6f6b4ca8b04a049cf
Saumya Ranjan Mohapatra <saumyaranjanmohapatra978@gmail.com>
2024-07-29 23:55:25 +0530

    Create Sample1.groovy

---
+++ Pipleines/Sample1.groovy
@@ -0,0 +1,32 @@
+// Jenkins Pipeline Script
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sh 'mvn deploy'
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Pipeline succeeded!'
+        }
+        failure {
+            echo 'Pipeline failed!'
+        }
+    }
+}
